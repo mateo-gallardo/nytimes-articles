e7ac0782c70a0980a965311a2da54385eab501e4
Mateo Gallardo <mateogallardo@gmail.com>
2018-10-14 10:23:40 -0300

    Initial project setup

---
+++ app/src/main/java/com/mateogallardo/nytimesarticles/ArticlesActivity.kt
@@ -0,0 +1,12 @@
+package com.mateogallardo.nytimesarticles
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ArticlesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_articles)
+    }
+}
